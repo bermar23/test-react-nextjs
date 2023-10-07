c595c9174cea403965e4ffc207ec993f06065d11
bermar.php.ph <bermar.php.ph@tripleonetech.net>
2023-10-07 11:59:53 +0800

    Add product item page

--- app/products/[id]/page.js
+++ app/products/[id]/page.js
@@ -1,8 +1,35 @@
-import Link from "@/components/Link"
+"use client";
 
-export default function Product({ params: { id } }){
-  return <div className="p-5 bg-slate-500">
-    <h1>this is product page of id {id}</h1>
-    <Link href="/products">Products</Link> 
-  </div>
+import Link from "@/components/Link";
+import { useState, useEffect } from "react";
+
+export default function Product({ params: { id } }) {
+  const [product, setProduct] = useState([]);
+
+  useEffect(() => {
+    //fetching can be done here
+    //this will be executed once mount
+    fetch(`https://fakestoreapi.com/products/${id}`)
+      .then((res) => res.json())
+      .then((json) => setProduct(json));
+  }, []);
+
+  if(!product) return null;
+
+  return (
+    <>
+      <div className="p-5 bg-slate-100 flex gap-4">
+        <Link href="/">Home</Link>
+        <Link href="/products">Products</Link>
+      </div>
+
+      <div className="flex flex-col gap-4 mt-4 p-4 bg-slate-100">
+          <span>id: {product.id}</span>
+          <span>title: {product.title}</span>
+          <span>price: {product.price}</span>
+          <span>category: {product.category}</span>
+          <span>description: {product.description}</span>
+        </div>
+    </>
+  );
 }
